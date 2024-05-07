3363b8fe1483fc4a9329b5da473f1a23180fae2f
govandoh <govandoh@miumg.edu.gt>
2024-05-06 23:30:30 -0600

    Implementacion metodo Neville

---
+++ Neville.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int n;
+float *colX, *colY, xint, fxint, valor; 
+
+void solicitar_datos(){
+    cout << "Ingrese la cantidad de datos: ";
+    cin >> n;
+
+    cout << "Ingrese el valor a interpolar xint: ";
+    cin >> xint;
+
+    colX = new  float[n];
+    colY = new   float [n];
+    for(int i=0; i<n; i++){
+        printf("Ingrese x(%3d), y(%3d): ", i,i); 
+        scanf("%f,%f", &colX[i],&colY[i]); 
+    }
+    
+}
+
+void ejecutarNeville(){
+    float Q[100][100];
+    valor = 0; 
+
+    for (int i = 0; i < n; i++) {
+        Q[i][i] = colY[i]; 
+    }
+
+    for (int i = 1; i < n; i++){
+        for (int j = 0; j <= n - i; j++) { 
+            Q[j][j + i] = ((xint - colX[j]) * Q[j + 1][j + i] - (xint - colX[j + i]) * Q[j][j + i - 1]) / (colX[j + i] - colX[j]);
+        }
+    }
+    valor = Q[0][n-1];
+    printf("Resultado a la interpolación del polinomio xint=%f es: %4.15f \n", xint, valor);
+}
+
+int main(){
+    solicitar_datos(); 
+    ejecutarNeville();
+    return 0;
+}
